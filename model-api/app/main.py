579e91dea4d8c9d56cb6e63326c5ddeba01ab27a
GAURAV05 <gauravgaur0504@gmail.com>
2025-06-29 01:48:26 +0530

    fixing deployment

--- model-api/app/main.py
+++ model-api/app/main.py
@@ -23,30 +23,31 @@ class_names = ["Brown Rust", "Healthy", "Yellow Rust"]
 def index():
     return jsonify(message="Welcome to the Wheat Disease Prediction API"), 200
 
-# @app.route('/predict', methods=['POST'])
-# def predict():
-#     if 'file' not in request.files:
-#         return jsonify(error="No file part in the request"), 400
+@app.route('/predict', methods=['POST'])
+def predict():
+    if 'file' not in request.files:
+        return jsonify(error="No file part in the request"), 400
 
-#     file = request.files['file']
-#     if file.filename == '':
-#         return jsonify(error="No file selected"), 400
+    file = request.files['file']
+    if file.filename == '':
+        return jsonify(error="No file selected"), 400
 
-#     try:
-#         image = Image.open(file)
-#         processed_image = preprocess_image(image, target_size=(256, 256))  
-#     except Exception as e:
-#         return jsonify(error="Error processing image"), 400
+    try:
+        image = Image.open(file)
+        processed_image = preprocess_image(image, target_size=(256, 256))  
+    except Exception as e:
+        return jsonify(error="Error processing image"), 400
 
-#     predictions = model.predict(processed_image)                       
-#     predicted_class_index = np.argmax(predictions, axis=1)[0]
-#     predicted_class_name = class_names[predicted_class_index]
-#     predicted_probability = predictions[0][predicted_class_index]
+    predictions = model.predict(processed_image)                       
+    predicted_class_index = np.argmax(predictions, axis=1)[0]
+    predicted_class_name = class_names[predicted_class_index]
+    predicted_probability = predictions[0][predicted_class_index]
 
-#     return jsonify({
-#         'predicted_class': predicted_class_name,
-#         'probability': float(predicted_probability)
-#     })
+    return jsonify({
+        'predicted_class': predicted_class_name,
+        'probability': float(predicted_probability)
+    })
 
-if __name__ == '__main__':
-    app.run(debug=True)
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))  # Use 5000 locally, $PORT on Railway
+    app.run(host="0.0.0.0", port=port)
