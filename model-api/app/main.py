a3f4c0a6e414feb8de1b7be489382afcfb0d8b2e
GAURAV05 <gauravgaur0504@gmail.com>
2025-06-29 00:10:39 +0530

    error handling and final changes

--- model-api/app/main.py
+++ model-api/app/main.py
@@ -9,22 +9,14 @@ import os
 
 app = Flask(__name__)
 CORS(app)
-# Get the directory of the current script
+
 current_dir = os.path.dirname(os.path.abspath(__file__))
-
-# Build the path to the model file
+# Load the model from the specified path
 file_path = os.path.join(current_dir, "..", "model", "model_wheat_vgg.h5")
-print(file_path)
-# Load the model using the file path
-
 model = load_model(file_path)
+print("Model loaded")
 
-# # Load the model
-# model = tf.keras.models.load_model('C:/Users/Gaurav Gaur/Documents/GitHub/Fasalmitra/model/model_wheat.h5')
-
-# Define the class names
-class_names = ["Brown Rust","Healthy","Yellow Rust"]
-
+class_names = ["Brown Rust", "Healthy", "Yellow Rust"]
 
 @app.route('/predict', methods=['POST'])
 def predict():
@@ -32,25 +24,24 @@ def predict():
         return jsonify(error="No file part in the request"), 400
 
     file = request.files['file']
-
     if file.filename == '':
-        return jsonify(error="No file selected for uploading"), 400
+        return jsonify(error="No file selected"), 400
 
-    # Process the file
-    image = Image.open(file)
-    processed_image = preprocess_image(image, target_size=(256, 256))
-    
-    # Get predictions
-    predictions = model.predict(processed_image)
+    try:
+        image = Image.open(file)
+        processed_image = preprocess_image(image, target_size=(256, 256))  
+    except Exception as e:
+        return jsonify(error="Error processing image"), 400
+
+    predictions = model.predict(processed_image)                       
     predicted_class_index = np.argmax(predictions, axis=1)[0]
     predicted_class_name = class_names[predicted_class_index]
     predicted_probability = predictions[0][predicted_class_index]
 
-    # Return the class name and the probability
     return jsonify({
         'predicted_class': predicted_class_name,
         'probability': float(predicted_probability)
     })
 
-if __name__ == '__main__':
-    app.run(debug=True)
+# if __name__ == '__main__':
+#     app.run(debug=True)
